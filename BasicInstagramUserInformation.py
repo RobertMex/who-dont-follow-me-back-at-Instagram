6d4b976ab76fc0c302ebbf0673ebbcc29cef76a8
RobertMex <70187422+RobertMex@users.noreply.github.com>
2023-02-11 16:07:58 -0600

    Clean BasicInstagramUserInformation python file

--- BasicInstagramUserInformation.py
+++ BasicInstagramUserInformation.py
@@ -1,6 +1,5 @@
 from typing import Optional
 import constants
-from logger import log
 
 class BasicInstagramUserInformation:
     def __init__(self, username: str) -> None:
@@ -12,21 +11,17 @@ class BasicInstagramUserInformation:
         self.following: list[BasicInstagramUserInformation] = []
 
     def getCleanFollowing(self) -> dict:
-        following = {}
-        for index in range(len(self.following)):
-            following[self.following[index].username] = {
-                constants.USERNAME: self.following[index].username,
-                constants.FULL_NAME: self.following[index].fullName,
-                constants.IS_PRIVATE: self.following[index].isPrivate
-            }
-        return following
+        return self.getCleanInstagramUsers(self.following)
 
     def getCleanFollower(self) -> dict:
-        follower = {}
-        for index in range(len(self.followers)):
-            follower[self.followers[index].username] = {
-                constants.USERNAME: self.followers[index].username,
-                constants.FULL_NAME: self.followers[index].fullName,
-                constants.IS_PRIVATE: self.followers[index].isPrivate
+        return self.getCleanInstagramUsers(self.followers)
+
+    def getCleanInstagramUsers(self, listUser: list) -> dict:
+        dictInstagramUsers: dict = {}
+        for index in range(len(listUser)):
+            dictInstagramUsers[listUser[index].username] = {
+                constants.USERNAME: listUser[index].username,
+                constants.FULL_NAME: listUser[index].fullName,
+                constants.IS_PRIVATE: listUser[index].isPrivate
             }
-        return follower
+        return dictInstagramUsers
